5524370814662a3358d1161075efcca3148d659b
muppudathiganesh <muppuraj11@gmail.com>
2025-09-02 13:05:22 +0530

    Fix: add STATICFILES_DIRS and custom static files

--- stationery_store/settings.py
+++ stationery_store/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [BASE_DIR / "static"] 
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # or os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
